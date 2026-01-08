2c1f150519e6c839f53ecb9e5a2f92ae6699e613
Your Name <your.email@example.com>
2026-01-08 14:20:24 +0900

    수정사항 반영

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Phone, MapPin } from 'lucide-react';
+import { MapPin, Fax, Mail } from 'lucide-react';
 
 export const Footer: React.FC = () => {
   return (
@@ -50,8 +50,12 @@ export const Footer: React.FC = () => {
                 <span className="leading-relaxed">경기도 화성시 동탄중심상가2길 26-15,<br/>8층 802-433B호(반송동, 지성프라자)</span>
               </li>
               <li className="flex items-center gap-4">
-                <Phone size={16} className="text-amber-500 shrink-0" />
-                <span className="text-white">010-9251-8022</span>
+                <Fax size={16} className="text-amber-500 shrink-0" />
+                <span className="text-white">0504 244 8022</span>
+              </li>
+              <li className="flex items-center gap-4">
+                <Mail size={16} className="text-amber-500 shrink-0" />
+                <span className="text-white">on-biz@gmail.com</span>
               </li>
             </ul>
           </div>
--- components/InquiryForm.tsx
+++ components/InquiryForm.tsx
@@ -158,3 +158,4 @@ export const InquiryForm: React.FC = () => {
   );
 };
 
+
